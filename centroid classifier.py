dd398c13a1b8bc5b86213696dde0bf0599fb1bba
Moritz-Apeldoorn <moritz.apeldoorn@stud.kswe.ch>
2022-08-31 18:31:39 +0200

    centroid classifier

--- centroid classifier.py
+++ centroid classifier.py
@@ -1,11 +1,9 @@
-import pandas, numpy, statistics
-data_set = pandas.read_csv("iris.csv")
-matrix_xy = data_set.iloc[0:150, 0:5].values
-matrix_x = data_set.iloc[0:150, 0:4].values
-vector_y = data_set.iloc[0:150, 4].values
-input_data = [1, 2, 3, 4]
+import pandas, numpy
+data_set = pandas.read_csv("iris.csv").values
+input_data = numpy.array([1, 2, 3, 4])
 labels = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
-center = [numpy.array([0, 0, 0, 0]), numpy.array([0, 0, 0, 0]), numpy.array([0, 0, 0, 0])]
-for element in matrix_xy:
-    center[labels.index(element[-1])] += numpy.array(element[0:4])
-print(center)
+center = [[numpy.array([0, 0, 0, 0]), 0], [numpy.array([0, 0, 0, 0]), 0], [numpy.array([0, 0, 0, 0]), 0]]
+for element in data_set:
+    index = labels.index(element[-1])
+    center[index] = [center[index][0] + numpy.array(element[0:4]), center[index][1] + 1]
+print(labels[numpy.argmin(list(map(lambda x: numpy.linalg.norm(x[0]/x[1]-input_data), center)))])
